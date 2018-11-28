2b763370480f07ca874ed9e86630e96311591b8c
Adam Duvick <aduvick@iastate.edu>
2018-11-27 22:43:22 -0600

    Added comments and a policy class with larger LSTM layers (unsure as to whether this is helpful yet)

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
-import multiprocessing
 import os
+import multiprocessing
 
+from stable_baselines.a2c.a2c import A2C
 from stable_baselines.common.policies import MlpLnLstmPolicy
-from stable_baselines.common.vec_env import SubprocVecEnv
-from stable_baselines import A2C
+from stable_baselines.common.vec_env.vec_normalize import VecNormalize
 
-from stable_baseline_wrapper import make_or_create_envs
+from stable_baseline_wrapper import VectorEnvironmentWrapper
 
-# specify model data
-model_type = A2C
-policy = MlpLnLstmPolicy
+
+# specify model type, policy type, define model name
+model_type, policy, model = A2C, MlpLnLstmPolicy, None
 env_id = 'HandManipulatePen-v0'
 model_name = f'{model_type.__name__}_{policy.__name__}_{env_id}'
 log = f'{model_name}_log'
-overwrite_previous_model = False
-retrain = False
+
+# specify model parameters
+model_kwargs = {'gamma': 0.99,              # default: 0.99
+                'n_steps': 5,               # default: 5
+                'vf_coef': 0.25,            # default: 0.25
+                'ent_coef': 0.01,           # default: 0.01
+                'max_grad_norm': 0.5,       # default: 0.5
+                'learning_rate': 2e-4,      # default: 7e-4
+                'alpha': 0.99,              # default: 0.99
+                'epsilon': 1e-5,            # default: 1e-5
+                'lr_schedule': 'linear',    # default: 'linear'
+                'verbose': 1,               # default: 0
+                'tensorboard_log': log,     # default: None
+                '_init_setup_model': True}  # default: True
+
+# specify whether or not to try to create a new model and/or train it
+overwrite_previous_model, train = True, True
 
 # get number of CPUs
 n_cpu = multiprocessing.cpu_count()
 
-# get pickled environments if they exist, else create them
-envs = make_or_create_envs(env_id, n_cpu, force_create=False)
-
 # initialize multiprocessor environment
-env = SubprocVecEnv([lambda: envs[i] for i in range(n_cpu)])
+env = VecNormalize(VectorEnvironmentWrapper(env_id, n_cpu))
 
+# create/load the model, maybe train it
 if overwrite_previous_model or not os.path.exists(f'{model_name}.pkl'):
-    model = model_type(policy, env, verbose=1, tensorboard_log=log)
+    model = model_type(policy, env, **model_kwargs)
     print(f'{model_name} created...')
-    model.save(model_name)
-    del model
-else:
+if not model:
+    model = model_type.load(model_name, env)
     print(f'Loading {model_name}...')
-
-model = model_type.load(model_name, env)
-
-if retrain:
+if train:
     print('Training model from last checkpoint')
-    model.learn(total_timesteps=10000)
+    model.learn(total_timesteps=100000, log_interval=100)
+    model.save(model_name)
 
+# render the env
 obs = env.reset()
 while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
+    print(rewards[0])
     env.render()
--- stable_baseline_wrapper.py
+++ stable_baseline_wrapper.py
@@ -1,23 +1,34 @@
-import os
-import pickle
 import gym
 import numpy as np
 
 from gym.spaces import Box, Dict
+from stable_baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
+from stable_baselines.common.policies import LstmPolicy
 
 
 class BaselineWrapper(gym.Wrapper):
+    '''Wrapper for a gym.core.GoalEnv so that it interfaces with
+    stable_baselines' API.
+
+    This wrapper flattens the observation, overrides the comuputed
+    reward and trims incoming actions before inputting them into self.step.'''
 
     def __init__(self, env_id):
+        '''Makes a wrapped gym.Env as specifiec by env_id.'''
         super().__init__(gym.make(env_id))
         self.observation_space = self._observation_space()
         self.action_space = self._action_space()
 
     def _action_space(self):
+        '''Get some information about this env and return the action_space.'''
         self.continuous_action_space = isinstance(self.env.action_space, Box)
         return self.env.action_space
 
     def _observation_space(self):
+        '''Get some information about this env. Return the untouched
+        observation_space if thie is env is not a GoalEnv, otherwise
+        return a Box with the same characteristics as the 3 Boxes
+        that compose this model.'''
         self.goal_env = isinstance(self.env.observation_space, Dict)
         if self.goal_env:
             space = list(self.env.observation_space.spaces.values())
@@ -28,18 +39,27 @@ class BaselineWrapper(gym.Wrapper):
             return self.env.observation_space
 
     def reset(self, **kwargs):
+        '''Return the flattened observation.'''
+        self.epsilon = .1
         return self.observation(self.env.reset(**kwargs))
 
     def step(self, action):
+        '''Execute given action and return overridden observation,
+        overridden reward, done, and info'''
         observation, reward, done, info = self.env.step(self.action(action))
-        return (self.observation(observation),
-                self.reward(reward, observation), done, info)
+        if done:
+            self.reset()
+        return (self.observation(observation), reward, done, info)
 
     def action(self, action):
-        if action.shape != self.action_space.shape:
-            action = action.reshape(self.action_space.shape)
+        '''Run epsilon-greedy algorithm where epsilon is derived from
+        the best action since the last reset. Trim and action if
+        necessary.'''
+        if self.goal_env and np.random.random() < self.epsilon:  # .1
+            return self.action_space.sample()
+
         if self.continuous_action_space:
-            low, high = self.env.action_space.low, self.env.action_space.high
+            low, high = self.action_space.low, self.action_space.high
             for i, _ in enumerate(action):
                 if low[i] >= action[i]:
                     action[i] = low[i]
@@ -48,6 +68,8 @@ class BaselineWrapper(gym.Wrapper):
         return action
 
     def observation(self, observation):
+        '''If this env is a GoalEnv, return the flattened observation,
+        otherwise return the original observation.'''
         if self.goal_env:
             observation_, achieved_goal, desired_goal = observation.values()
             return np.append(observation_, [achieved_goal, desired_goal])
@@ -55,26 +77,76 @@ class BaselineWrapper(gym.Wrapper):
             return observation
 
     def reward(self, reward, observation):
+        '''If this env is a GoalEnv, calculate the overridden reward,
+        else return the reward.'''
         if self.goal_env:
             _, achieved, desired = observation.values()
-            return 1 - (np.square(desired - achieved)).mean(axis=0) + reward
+#             rms = np.sqrt(np.mean(np.square(desired - achieved)))
+            reward += sum(1 - abs(desired - achieved))
+            return reward
         else:
             return reward
 
 
-def make_or_create_envs(env_id, how_many, force_create=False):
-    pickled_envs_file = f'{how_many}_pickled_{env_id}_envs'
-    if os.path.exists(pickled_envs_file) and not force_create:
-        envs = pickle.load(open(pickled_envs_file, 'rb'))
-        print(f'{how_many} {env_id} environments have been loaded.')
-    else:
-        envs = [BaselineWrapper(env_id) for _ in range(how_many)]
-        pickle.dump(envs, open(pickled_envs_file, 'wb'))
-        print(f'{how_many} {env_id} new environments have been created and pickled.')
-    return envs
+class VectorEnvironmentWrapper(SubprocVecEnv):
+    '''Wrapper for SubprocVecEnv to alter render functionality'''
+
+    def __init__(self, env_id, n_env):
+        '''Initializes object from super class.'''
+        super().__init__([lambda: BaselineWrapper(env_id) for _ in range(n_env)])
+
+    def render(self, number=1, tiled=False, mode='human', *args, **kwargs):
+        '''Override render to add functionality of choosing many env
+        should be displayed on screen. Also add boolean as to whether
+        or not a tiled image should be put together and displayed.'''
+        number = len(self.remotes) if number > len(self.remotes) else number
+        for i in range(number):
+            # gather images from subprocesses
+            # `mode` will be taken into account later
+            self.remotes[i].send(('render', (args, {'mode': 'rgb_array', **kwargs})))
+        imgs = [self.remotes[i].recv() for i in range(number)]
+        if tiled:
+            # Create a big image by tiling images from subprocesses
+            from stable_baselines.common.tile_images import tile_images
+            bigimg = tile_images(imgs)
+            if mode == 'human':
+                import cv2
+                cv2.imshow('vecenv', bigimg[:, :, ::-1])
+                cv2.waitKey(1)
+            elif mode == 'rgb_array':
+                return bigimg
+
+
+class LargeMlpLnLstmPolicy(LstmPolicy):
+    """
+    Policy object that implements actor critic, using a layer normalized LSTMs with a MLP feature extraction
+
+    :param sess: (TensorFlow session) The current TensorFlow session
+    :param ob_space: (Gym Space) The observation space of the environment
+    :param ac_space: (Gym Space) The action space of the environment
+    :param n_env: (int) The number of environments to run
+    :param n_steps: (int) The number of steps to run for each environment
+    :param n_batch: (int) The number of batch to run (n_envs * n_steps)
+    :param n_lstm: (int) The number of LSTM cells (for recurrent policies)
+    :param reuse: (bool) If the policy is reusable or not
+    :param kwargs: (dict) Extra keyword arguments for the nature CNN feature extraction
+    """
+
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse=False, **_kwargs):
+        super().__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, n_lstm=512, reuse=reuse,
+                         layers=[128, 128], layer_norm=True, feature_extraction="mlp", **_kwargs)
 
 
 if __name__ == '__main__':
     env = BaselineWrapper('HandManipulatePen-v0')
 
-    print(env.unwrapped)
+    env.reset()
+
+    while True:
+        action = env.action_space.sample()
+
+        observation, reward, done, info = env.step(action)
+
+        print(reward, env.epsilon)
+
+        env.render()
